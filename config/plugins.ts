510ccc362f242de38450930bd4e8e89494332984
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-07-26 21:39:35 +0300

    jwt secret

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+interface PluginsConfig {
+  "users-permissions"?: {
+    config?: {
+      jwtSecret: string;
+    };
+  };
+}
+
+export default ({
+  env,
+}: {
+  env: (key: string, defaultValue?: string) => string;
+}) => {
+  return {
+    "users-permissions": {
+      config: {
+        jwtSecret: env("JWT_SECRET", "your-default-jwt-secret"),
+      },
+    },
+  } as PluginsConfig;
+};
